8acbe4c02e631023a422c744775a1305a096b4a3
Neyan20 <npflor20@gmail.com>
2026-07-05 21:22:49 +0800

    initialized variables and methods for inventory

--- Inventory.java
+++ Inventory.java
@@ -1,3 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Inventory {
-    
+    Map<String, Integer> fruits = new HashMap<>();
+    Map<String, Integer> bases = new HashMap<>();
+
+    public void addItem(String name, int qty) {
+        
+    }
+
+    public boolean removeItem(String name, int qty) {
+
+    }
+
+    public boolean hasItem(String name, int qty) {
+
+    }
+
+    public int getQuantity(String name) {
+
+    }
+
+    public Map<String, Integer> getFruits() {
+        return fruits;
+    }
+
+    public Map<String, Integer> getBases() {
+        return bases;
+    }
+
+    public void display() {
+
+    }
+
+    public void setDefaults() {
+
+    }
 }
